41baa1fe1016f17a15a8bb4909bbd92e0aa92f61
tdostilio <tdostilio@gmail.com>
2017-08-07 11:45:19 -0400

    live connection established and made a generic re-usable i/o script

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const krispykreme = require('./krispykreme');
 const server = require('./server')
 
 function main() {
-  krispykreme.addHotListener(server.broadcast('get deez donuts'));
-  krispykreme.addNoneListener(server.broadcast('shit outta luck'));
+  server.init((socket) => {
+    socket.send('You are now connected')
+    console.log('Someone has connected')
+  });
+  krispykreme.addHotListener( () => {
+      server.broadcast('get deez donuts');
+  })
+  krispykreme.addNoneListener(()=> {
+      server.broadcast('shit outta luck')
+    });
 }
 main();
